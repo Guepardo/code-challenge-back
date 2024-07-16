691a7f9924cc0e293a09d50b4d15a9ae241f8d0b
Allyson Maciel Guimarães <bsinet@hotmail.com>
2024-07-16 16:57:59 -0300

    feat: add profile model

---
+++ spec/factories/profiles.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :profile do
+    username { Faker::Name.unique.name }
+    profile_url { "https://github.com/#{username}" }
+    avatar_url { Faker::Internet.url }
+    fellows_count { Faker::Number.within(range: 0..100) }
+    fallowing_count { Faker::Number.within(range: 0..100) }
+    stars_count { Faker::Number.within(range: 0..100) }
+    year_contributions_count { Faker::Number.within(range: 0..100) }
+    location { Faker::Address.city }
+    organization_name { Faker::Company.name }
+  end
+end
