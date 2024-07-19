735d4438bb428e540c9601ac837d300a321a5e27
Allyson Maciel Guimarães <bsinet@hotmail.com>
2024-07-19 15:21:16 -0300

    chore: create seed records

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+require 'securerandom'
+
+# Definindo o número de perfis que você deseja criar
+number_of_profiles = 100
+
+number_of_profiles.times do
+  Profile.create(
+    username: Faker::Name.unique.name,
+    profile_url: "https://github.com/#{SecureRandom.hex(10)}",
+    avatar_url: Faker::Internet.url,
+    followers_count: Faker::Number.within(range: 0..100),
+    fallowing_count: Faker::Number.within(range: 0..100),
+    stars_count: Faker::Number.within(range: 0..100),
+    year_contributions_count: Faker::Number.within(range: 0..100),
+    location: Faker::Address.city,
+    organization_name: Faker::Company.name
+  )
+end
